ebb6253edbbd29d25fd03b53edac5a4d32b12afc
Hesham <61054665+h9-tect@users.noreply.github.com>
2023-01-07 18:03:55 +0200

    Create eval.py

---
+++ eval.py
@@ -0,0 +1,27 @@
+import torch
+
+import torch
+
+from transformer import Transformer
+from data_utils import create_dataloader
+
+def evaluate_bleu(model, dataloader, src_field, tgt_field, device):
+    model.eval()
+    bleu_score = 0.
+    with torch.no_grad():
+        for src, tgt in dataloader:
+            src, tgt = src.to(device), tgt.to(device)
+            src_mask, tgt_mask = create_mask(src, tgt)
+            output = model.predict(src, src_mask, tgt_mask, max_length=tgt.size(1))
+            output_str = [tgt_field.vocab.itos[i] for i in output]
+            tgt_str = [tgt_field.vocab.itos[i] for i in tgt[:, 1:].squeeze(0)]
+            bleu_score += bleu(output_str, tgt_str)
+            
+    return bleu_score / len(dataloader)
+
+def bleu(output_str, tgt_str):
+    # Calculate BLEU score
+    # ...
+    
+    return score
+
